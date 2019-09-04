2d69cbccefa83e92518b3d18a53e0d7c1005f71c
Lming1 <spear131121@gmail.com>
2019-09-04 19:37:23 +0900

    protocol extension2

---
+++ 0904/foobar_protocol_extension.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Foundation
+import AppKit
+
+
+
+struct Particle {
+    var name: String
+    var symbol: String
+    var statistics: String
+    
+}
+
+// 배열 리터럴 문법 사용해서 구현한 예제 ..
+
+
+struct ParticleList: ExpressibleByArrayLiteral {
+    private let items: [Particle]
+    init(arrayLiteral: Particle...) {
+        self.items = arrayLiteral
+    }
+}
+
+
+var test1 = Particle(name: "test1", symbol: "q", statistics: "test1")
+var test2 = Particle(name: "test2", symbol: "w", statistics: "test2")
+var test3 = Particle(name: "test3", symbol: "e", statistics: "test3")
+
+var particleList = [test1, test2, test3]
+
+
